b72b7760cd9095ce1f22b794f3b618b9321b2407
Fernando Nina <Fernandonina611@gmail.com>
2024-08-22 12:07:53 -0400

    feat: :sparkles: agregado el end point para crear Encuestas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\SurveyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('/surveys', SurveyController::class);
